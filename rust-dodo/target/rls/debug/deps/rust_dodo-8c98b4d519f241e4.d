4862e53e7ccdf5573284bb5dcdf85bb7ee31b45e
Patrick McLennan <patrick.a.mclennan@gmail.com>
2020-07-24 23:35:29 -0400

    Folder Structure

--- rust-dodo/target/rls/debug/deps/rust_dodo-8c98b4d519f241e4.d
+++
@@ -1,5 +0,0 @@
-/Users/patrickmclennan/Documents/react_yew/rust-dodo/target/rls/debug/deps/rust_dodo-8c98b4d519f241e4.rmeta: src/main.rs
-
-/Users/patrickmclennan/Documents/react_yew/rust-dodo/target/rls/debug/deps/rust_dodo-8c98b4d519f241e4.d: src/main.rs
-
-src/main.rs:
--- rust-dodo/target/rls/debug/deps/rust_dodo-ef05ff491015d961.d
+++
@@ -1,5 +0,0 @@
-/Users/patrickmclennan/Documents/react_yew/rust-dodo/target/rls/debug/deps/rust_dodo-ef05ff491015d961.rmeta: src/main.rs
-
-/Users/patrickmclennan/Documents/react_yew/rust-dodo/target/rls/debug/deps/rust_dodo-ef05ff491015d961.d: src/main.rs
-
-src/main.rs:
